2abe3dfcf9650f9f866e8afd89494c77669c729f
RGTvertex-web <safalsharmayt@gmail.com>
2026-07-16 20:33:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,11 @@ csrf = CSRFProtect()
 limiter = Limiter(key_func=get_remote_address)
 
 
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 def create_app():
     app = Flask(__name__)
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
     app.config.from_object(get_config())
 
     # ── Logging ────────────────────────────────────────────────────────────────
